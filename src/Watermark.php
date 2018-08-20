9a5fadc0b03d536a11022e8e2232093be7d0eb96
Rafael Moreno <alejandro8924@gmail.com>
2018-08-20 15:15:47 -0500

    Provider y facade, autodescubrir

--- src/Watermark.php
+++ src/Watermark.php
@@ -8,36 +8,15 @@ namespace rafaelmorenojs\Watermark;
 class Watermark
 {
 	/**
-	 * Ruta del directorio de la stampa que queremos usar como marca de agua
-	 * @var string
-	 */
-	private  $stamp;
-
-	/**
-	 * Ruta del directorio de la imagen a la que queremos agregarle la marca de agua
-	 * @var [type]
-	 */
-	private $image;
-
-	/**
-	 * [__construct description]
-	 * @param string $stamp [description]
-	 * @param string $image [description]
-	 */
-	function __construct(string $stamp, string $image)
-	{
-		$this->stamp = $stamp;
-		$this->image = $image;
-	}
-
-	/**
 	 * [render description]
-	 * @return [type] [description]
+	 * @param  string $stamp [description]
+	 * @param  string $image [description]
+	 * @return [type]        [description]
 	 */
-	public function render()
+	public function render(string $stamp, string $image)
 	{
-		$stamp = imagecreatefrompng($this->stamp);
-		$image = imagecreatefromjpeg($this->image);
+		$stamp = imagecreatefrompng($stamp);
+		$image = imagecreatefromjpeg($image);
 
 		$rightMargin = 10; // MARGEN DERECHO
 		$lowerMargin = 10; // MARGEN INFERIOR
